90d0ee083ae4f53bc8686179f39b62ad861cfbd3
Kent Frazier <kentfrazier@gmail.com>
2014-02-12 16:54:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,18 @@ from setuptools import (
 setup(
     name='parsnip',
     version='0.1.0',
-    description='Python parsing and manipulation',
+    description='Library for parsing and manipulating Python code',
+    long_description='Library for parsing and manipulating Python code',
     author='Kent Frazier',
     author_email='kentfrazier@gmail.com',
     url='https://github.com/kentfrazier/parsnip',
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
 )
