a264aa7797b54cb752ce6fdd543b6778475dac27
Kent Frazier <kentfrazier@gmail.com>
2014-02-12 15:32:19 -0600

    Update serializers to show tokens

--- parsnip/serialize.py
+++ parsnip/serialize.py
@@ -7,20 +7,22 @@ try:
     StringIO
 except ImportError:
     from StringIO import StringIO
+import tokenize
 from xml.etree.ElementTree import (
     ElementTree,
     TreeBuilder,
 )
 
 
-types = namedtuple('types', 'AST CST TOKENS BYTECODES DEFAULT ALL')(
+types = namedtuple('types', 'AST TOKENS SYMTABLE DEFAULT ALL')(
     AST=1,
-    CST=2,
-    TOKENS=4,
-    BYTECODES=8,
+    TOKENS=2,
+    SYMTABLE=4,
     DEFAULT=1 | 2 | 4,  # no BYTECODES by default, since it requires compiling
-    ALL=1 | 2 | 4 | 8,
+    ALL=1 | 2 | 4,
 )
+
+# TODO: add handling for parse tree and disassembled byte code
 
 
 class ModuleSerializer(ast.NodeVisitor):
@@ -31,13 +33,35 @@ class ModuleSerializer(ast.NodeVisitor):
         serializer.run()
         return serializer
 
-    def __init__(self, module):
+    def __init__(self, module, flags=types.DEFAULT):
         self.module = module
+        self.flags = flags
 
     def run(self):
+        if self.flags & types.AST:
+            self.run_ast()
+        if self.flags & types.TOKENS:
+            self.run_tokens()
+        if self.flags & types.SYMTABLE:
+            self.run_symtable()
+
+    def run_ast(self):
         self.visit(self.module.ast)
 
+    def run_tokens(self):
+        for offset, token in enumerate(self.module.tokens):
+            self.process_token(self.normalize_token(token), offset)
+
+    def run_symtable(self):
+        pass  # TODO
+
+    def normalize_token(self, token):
+        return token + (None,) * (5 - len(token))
+
     def get_root(self):
+        raise NotImplementedError()
+
+    def process_token(self, token, offset):
         raise NotImplementedError()
 
     def start_ast(self, ast_node):
@@ -89,17 +113,46 @@ class ModuleSerializer(ast.NodeVisitor):
 
 class DictModuleSerializer(ModuleSerializer):
 
-    def __init__(self, module):
-        super(DictModuleSerializer, self).__init__(module)
+    def __init__(self, module, flags=types.DEFAULT):
+        super(DictModuleSerializer, self).__init__(module, flags)
+        self.tokens = []
         self.stack = []
-        self.root = None
-        self.handlers = [self.set_root]
+        self.root = {}
+        self.handlers = []
 
-    def set_root(self, root):
-        self.root = root
+    def run_ast(self):
+        self.handlers.append(partial(self.root.__setitem__, 'ast'))
+        super(DictModuleSerializer, self).run_ast()
+        self.handlers.pop()
+
+    def run_tokens(self):
+        self.root['tokens'] = self.tokens
+        super(DictModuleSerializer, self).run_tokens()
 
     def get_root(self):
         return self.root
+
+    def process_token(self, token, offset):
+        ttype, tval, tstart, tend, tline = token
+        t_dict = {
+            'type_name': tokenize.tok_name[ttype],
+            'type': ttype,
+            'value': tval,
+            'offset': offset,
+        }
+        if tstart is not None:
+            t_dict.update({
+                'start_line': tstart[0],
+                'start_col': tstart[1],
+            })
+        if tend is not None:
+            t_dict.update({
+                'end_line': tend[0],
+                'end_col': tend[1],
+            })
+        if tline is not None:
+            t_dict['line'] = tline
+        self.tokens.append(t_dict)
 
     def process_field(self, field, value):
         self.handlers.append(partial(self.stack[-1].__setitem__, field))
@@ -119,6 +172,10 @@ class DictModuleSerializer(ModuleSerializer):
         col = getattr(node, 'col_offset', None)
         if col is not None:
             node_dict['__col__'] = col
+        if self.flags & types.TOKENS:
+            n = self.module.ast_map.get(node)
+            if n is not None and n.token_start < n.token_end:
+                node_dict['__tokens__'] = [n.token_start, n.token_end]
         self.handle(node_dict)
         self.stack.append(node_dict)
 
@@ -159,19 +216,54 @@ ElementBuilder = namedtuple('ElementBuilder', 'tag attrs children')
 
 class EtreeModuleSerializer(ModuleSerializer):
 
-    def __init__(self, module):
-        super(EtreeModuleSerializer, self).__init__(module)
-        self.stack = [ElementBuilder('root', {}, [])]
+    def __init__(self, module, flags=types.DEFAULT):
+        super(EtreeModuleSerializer, self).__init__(module, flags)
+        self.stack = [ElementBuilder('module', {}, [])]
         self.builder = TreeBuilder()
         self.etree = None
+
+    def run_ast(self):
+        ast_el = ElementBuilder('ast', {}, [])
+        self.add_child(ast_el)
+        self.stack.append(ast_el)
+        super(EtreeModuleSerializer, self).run_ast()
+        self.stack.pop()
+
+    def run_tokens(self):
+        tokens_el = ElementBuilder('tokens', {}, [])
+        self.add_child(tokens_el)
+        self.stack.append(tokens_el)
+        super(EtreeModuleSerializer, self).run_tokens()
+        self.stack.pop()
 
     def get_root(self):
         if self.etree is None:
             assert len(self.stack) == 1
-            assert len(self.stack[0].children) == 1
-            self._build(self.stack[0].children[0])
+            self._build(self.stack[0])
             self.etree = ElementTree(self.builder.close())
         return self.etree
+
+    def process_token(self, token, offset):
+        ttype, tval, tstart, tend, tline = token
+        attrs = {
+            'type': repr(ttype),
+            'value': repr(tval).lstrip('u')[1:-1],
+            'offset': repr(offset),
+        }
+        if tstart is not None:
+            attrs.update({
+                'start_line': repr(tstart[0]),
+                'start_col': repr(tstart[1]),
+            })
+        if tend is not None:
+            attrs.update({
+                'end_line': repr(tend[0]),
+                'end_col': repr(tend[1]),
+            })
+        if tline is not None:
+            attrs['line'] = repr(tline).lstrip('u')[1:-1]
+        el = ElementBuilder(tokenize.tok_name[ttype], attrs, [])
+        self.add_child(el)
 
     def add_attr(self, attr, value):
         self.stack[-1].attrs[attr] = value
@@ -187,6 +279,12 @@ class EtreeModuleSerializer(ModuleSerializer):
         col = getattr(node, 'col_offset', None)
         if col is not None:
             el.attrs['col'] = repr(col)
+        if self.flags & types.TOKENS:
+            n = self.module.ast_map.get(node)
+            if n is not None:
+                if n.token_start < n.token_end:
+                    el.attrs['token_start'] = repr(n.token_start)
+                    el.attrs['token_end'] = repr(n.token_end)
         self.add_child(el)
         self.stack.append(el)
 
